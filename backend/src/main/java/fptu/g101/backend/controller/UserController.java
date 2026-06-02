25cbc0bb4f8f54029a139af5fd63024d13390055
Kiennt1152 <kiennthe163659@fpt.edu.vn>
2026-06-02 20:14:16 +0700

    chore: inital project structure

---
+++ backend/src/main/java/fptu/g101/backend/controller/UserController.java
@@ -0,0 +1,8 @@
+package fptu.g101.backend.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+}
