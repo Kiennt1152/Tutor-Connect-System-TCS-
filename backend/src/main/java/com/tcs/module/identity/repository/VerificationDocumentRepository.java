9b3b4bf30597c91bd152b2274327fc1f8dd1e7ed
NguyenHK186858 <nguyenhkhe186858@fpt.edu.vn>
2026-07-02 10:20:23 +0700

    create feature verification-review screen
    create feature verification-review screen

--- backend/src/main/java/com/tcs/module/identity/repository/VerificationDocumentRepository.java
+++ backend/src/main/java/com/tcs/module/identity/repository/VerificationDocumentRepository.java
@@ -1,9 +1,12 @@
 package com.tcs.module.identity.repository;
 
 import com.tcs.module.identity.entity.VerificationDocument;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VerificationDocumentRepository extends JpaRepository<VerificationDocument, Long> {
+
+    List<VerificationDocument> findByVerificationRequest_VerificationId(Long verificationId);
 }
